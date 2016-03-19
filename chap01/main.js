4199041feb0495c4c53d4b211dff3819bb58c77c
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-03-19 08:51:37 -0300

    Javascript Learning - Hola Mundo

--- chap01/main.js
+++ chap01/main.js
@@ -14,5 +14,12 @@ $(document).ready(function(){
         var c = Shape.Circle(event.point, 20);
         c.fillColor = 'green';
     };
+    var circle = Shape.Circle(200, 200, 80);
+    circle.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'hello world';
     paper.view.draw();//le indico que dibuje
 });
