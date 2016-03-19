6817f6ddb22710472b57071fe1182ed93209f4be
Alejandro Antivero <alejandro.antivero@gmail.com>
2016-03-19 08:05:06 -0300

    Javascript Learning - Utilizando un elemento Canvas y Paper.js

--- chap01/main.js
+++ chap01/main.js
@@ -5,4 +5,11 @@
 $(document).ready(function(){
     'use strict';
     console.log('main.js loaded');
+
+    paper.install(window);//instala la libreria a nivel global
+    paper.setup(document.getElementById('mainCanvas'));//atacha paper al canvas
+    //componentes del dibujo
+    var c = Shape.Circle(200, 200, 50);
+    c.fillColor = 'green';
+    paper.view.draw();//le indico que dibuje
 });
